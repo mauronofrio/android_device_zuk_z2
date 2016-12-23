4e8788c88369fa931b15a871230d04d3714ea8d1
mauronofrio matarrese <poronofrio@gmail.com>
2016-12-23 23:28:41 +0100

    Add Twrp support

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+        $(LOCAL_DIR)/cm_z2.mk
+
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,3 +199,16 @@ BOARD_USES_QC_TIME_SERVICES := true
 
 # inherit from the proprietary version
 -include vendor/zuk/z2/BoardConfigVendor.mk
+
+#twrp
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+BOARD_HAS_FLIPPED_SCREEN := true
+PRODUCT_COPY_FILES += device/zuk/z2/twrp.fstab:recovery/root/etc/twrp.fstab
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_DEVICE_MODULES := libbinder libgui libui libEGL libGLES_trace libGLESv2 libprotobuf-cpp-lite libsync # twrpdec
+TW_RECOVERY_ADDITIONAL_RELINK_FILES := $(OUT)/system/lib64/libbinder.so $(OUT)/system/lib64/libgui.so $(OUT)/system/lib64/libui.so $(OUT)/system/lib64/libEGL.so $(OUT)/system/lib64/libGLES_trace.so $(OUT)/system/lib64/libGLESv2.so $(OUT)/system/lib64/libprotobuf-cpp-lite.so $(OUT)/system/lib64/libsync.so # $(OUT)/recovery/root/sbin/twrpdec
+#TWRP_INCLUDE_LOGCAT := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_CRYPTO := true
