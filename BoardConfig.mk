67f0070a9eb9e7ca6219200624e4985801e78300
mauronofrio matarrese <poronofrio@gmail.com>
2017-01-02 22:44:40 +0100

    Remove Download Button

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -210,5 +210,5 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_RECOVERY_DEVICE_MODULES := libbinder libgui libui libEGL libGLES_trace libGLESv2 libprotobuf-cpp-lite libsync # twrpdec
 TW_RECOVERY_ADDITIONAL_RELINK_FILES := $(OUT)/system/lib64/libbinder.so $(OUT)/system/lib64/libgui.so $(OUT)/system/lib64/libui.so $(OUT)/system/lib64/libEGL.so $(OUT)/system/lib64/libGLES_trace.so $(OUT)/system/lib64/libGLESv2.so $(OUT)/system/lib64/libprotobuf-cpp-lite.so $(OUT)/system/lib64/libsync.so # $(OUT)/recovery/root/sbin/twrpdec
 #TWRP_INCLUDE_LOGCAT := true
-TW_HAS_DOWNLOAD_MODE := true
+#TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_CRYPTO := true
